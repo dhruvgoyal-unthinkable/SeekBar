b0d837e886a8c2587289b6006e84fd80992ab823
daffolap-932 <anoushka.singh@unthinkabel.co>
2021-04-14 13:10:19 +0530

    seekbar added

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.seekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+import com.example.seekbar.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    int progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress = binding.seekBar.getProgress();
+                binding.seekBar.setProgress(progress + 1);
+            }
+        });
     }
 }
