aedf1c021a33e9ceb81a5da5589c6b563814bffa
unthinkable-lap-70 <dhruv.goyal@unthinkable.co>
2021-04-14 15:36:49 +0530

    Added custom SeekBar

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.seekbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.seekbar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     int progress;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                progress = binding.seekBar.getProgress();
-                binding.seekBar.setProgress(progress + 1);
-            }
+        binding.nextBtn.setOnClickListener(view -> {
+            progress = binding.seekBar.getProgress(); binding.seekBar.setProgress(progress + 1);
         });
     }
 }
