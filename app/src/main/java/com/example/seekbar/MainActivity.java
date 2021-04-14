96565fecd8275614e248169864f0880ff8f0a0d6
daffolap-932 <anoushka.singh@unthinkabel.co>
2021-04-14 13:35:13 +0530

    changes in seekbar :- added next button

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.seekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.seekbar.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    int progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progress = binding.seekBar.getProgress();
+                binding.seekBar.setProgress(progress + 1);
+            }
+        });
     }
 }
