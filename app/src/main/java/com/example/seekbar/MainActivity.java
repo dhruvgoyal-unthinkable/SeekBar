f6a938b5e2d7c8e5fe884f58bc8e249023ee00b0
daffolap-932 <anoushka.singh@unthinkabel.co>
2021-04-15 11:09:45 +0530

    recycler view added

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.seekbar;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.seekbar.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    ArrayList<Images> imagesList;
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+    ProgressAdapter adapter;
+    LinearLayoutManager HorizontalLayout;
+    View ChildView;
+    int RecyclerViewItemPosition;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -17,6 +29,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        RecyclerViewLayoutManager
+                = new LinearLayoutManager(
+                getApplicationContext());
+        binding.recyclerView.setLayoutManager(RecyclerViewLayoutManager);
+        AddItemsToRecyclerViewArrayList();
+        adapter = new ProgressAdapter(imagesList);
+        HorizontalLayout
+                = new LinearLayoutManager(
+                MainActivity.this,
+                LinearLayoutManager.HORIZONTAL,
+                false);
+        binding.recyclerView.setLayoutManager(HorizontalLayout);
+        binding.recyclerView.setAdapter(adapter);
 
+    }
+
+    public void AddItemsToRecyclerViewArrayList()
+    {   int pos = 4;
+        // Adding items to ArrayList
+        imagesList = new ArrayList<>();
+        for(int i=0; i< pos ;i++){
+            Images images = new Images();
+            images.setProgress_circular(R.drawable.ic_default_thumb);
+            images.setProgress_line(R.drawable.ic_progress_line);
+            imagesList.add(images);
+        }
     }
 }
---
+++ app/src/main/java/com/example/seekbar/ProgressAdapter.java
@@ -0,0 +1,61 @@
+package com.example.seekbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProgressAdapter extends RecyclerView.Adapter<ProgressAdapter.MyView>{
+
+    private ArrayList<Images> imagesList;
+
+    public class MyView
+            extends RecyclerView.ViewHolder {
+
+        ImageView progress_circular, progress_line;
+
+
+        public MyView(View view)
+        {
+            super(view);
+            progress_circular = (ImageView)view.findViewById(R.id.progress_circular);
+            progress_line = (ImageView) view.findViewById(R.id.progress_line);
+        }
+    }
+    public ProgressAdapter(ArrayList<Images> imageList)
+    {
+        this.imagesList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public MyView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView
+                = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item,
+                        parent,
+                        false);
+
+        return new MyView(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyView holder, int position) {
+        holder.progress_circular.setImageResource(imagesList.get(position).getProgress_circular());
+        holder.progress_line.setImageResource(imagesList.get(position).getProgress_line());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagesList.size();
+    }
+}
