dd6a04b57dd3aeeb2acbb05b3f26e1ae0511aa85
unthinkable-lap-70 <dhruv.goyal@unthinkable.co>
2021-04-15 13:12:55 +0530

    Added custom ProgressIndicator

--- app/src/main/java/com/example/seekbar/ProgressIndicator/ProgressAdapter.java
+++ app/src/main/java/com/example/seekbar/ProgressIndicator/ProgressAdapter.java
@@ -1,5 +1,6 @@
 package com.example.seekbar.ProgressIndicator;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +12,23 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.seekbar.R;
+import com.example.seekbar.databinding.ItemBinding;
 
-public class ProgressAdapter extends RecyclerView.Adapter<ProgressAdapter.MyView>{
+public class ProgressAdapter extends RecyclerView.Adapter<ProgressAdapter.MyView> {
 
-    private ArrayList<Images> imagesList;
+    private final ArrayList<Drawable> icons;
 
-    public class MyView
-            extends RecyclerView.ViewHolder {
+    public static class MyView extends RecyclerView.ViewHolder {
+        ImageView icon;
 
-        ImageView progress_circular, progress_line;
-
-
-        public MyView(View view)
-        {
+        public MyView(View view) {
             super(view);
-            progress_circular = view.findViewById(R.id.progress_circular);
-            progress_line = view.findViewById(R.id.progress_line);
+            icon = view.findViewById(R.id.icon);
         }
     }
-    public ProgressAdapter(ArrayList<Images> imageList)
-    {
-        this.imagesList = imageList;
+
+    public ProgressAdapter(ArrayList<Drawable> icons) {
+        this.icons = icons;
     }
 
     @NonNull
@@ -45,19 +42,23 @@ public class ProgressAdapter extends RecyclerView.Adapter<ProgressAdapter.MyView
                         parent,
                         false);
 
+
         return new MyView(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyView holder, int position) {
-        holder.progress_circular.setImageResource(imagesList.get(position).getProgress_circular());
-        holder.progress_line.setImageResource(imagesList.get(position).getProgress_line());
+        holder.icon.setImageDrawable(icons.get(position));
+    }
 
 
+    @Override
+    public int getItemViewType(int position) {
+        return position;
     }
 
     @Override
     public int getItemCount() {
-        return imagesList.size();
+        return icons.size();
     }
 }
--- app/src/main/java/com/example/seekbar/ProgressIndicator/ProgressIndicator.java
+++ app/src/main/java/com/example/seekbar/ProgressIndicator/ProgressIndicator.java
@@ -1,16 +1,55 @@
 package com.example.seekbar.ProgressIndicator;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.seekbar.R;
 
-public class ProgressIndicator extends View {
+import java.util.ArrayList;
+
+public class ProgressIndicator extends LinearLayout {
+
+    private final ArrayList<Drawable> icons;
+    private int progress;
+    private final Context context;
+
     public ProgressIndicator(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        inflate(context, R.layout.progress_indicator, null);
+        this.context = context;
+        icons = new ArrayList<>();
+        progress = 0;
+        inflate(context, R.layout.progress_indicator, this);
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.ProgressIndicator);
+        loadIcons(attributes);
+        updateRecyclerView();
+        attributes.recycle();
+    }
+
+    private void updateRecyclerView() {
+        ProgressAdapter adapter = new ProgressAdapter(icons);
+        RecyclerView progressBar = findViewById(R.id.progress_bar);
+        progressBar.setHasFixedSize(true);
+        progressBar.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        progressBar.setAdapter(adapter);
+    }
+
+    private void loadIcons(TypedArray attributes) {
+        for (int i = 0; i < progress; i++) {
+            icons.add(attributes.getDrawable(R.styleable.ProgressIndicator_processedIcon));
+            icons.add(attributes.getDrawable(R.styleable.ProgressIndicator_processedProgressLine));
+        }
+        icons.add(attributes.getDrawable(R.styleable.ProgressIndicator_progressIcon));
+        for (int i = progress + 1; i < attributes.getInt(R.styleable.ProgressIndicator_max, 3); i++) {
+            icons.add(attributes.getDrawable(R.styleable.ProgressIndicator_defaultProgressLine));
+            icons.add(attributes.getDrawable(R.styleable.ProgressIndicator_defaultIcon));
+        }
     }
 }
